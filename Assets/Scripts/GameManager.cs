abff4976db1e4642f3f701f60f4255af8637ac6e
GalacticKatAlpha <524721@chrome.escg.ac.uk>
2021-12-04 01:10:36 +0000

    Updated for menu system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
 
     // References
     public Player player;
-    // public weapon weapon..
+    public Weapon weapon;
     public FloatingTextManager floatingTextManager;
 
     // Logic
@@ -38,6 +38,22 @@ public class GameManager : MonoBehaviour
     public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
     {
         floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
+
+    //Upgrade Weapon
+    public bool TryUpgradeWeapon()
+    {
+        // Is the weapon max level?
+        if (weaponPrices.Count <= weapon.weaponLevel)
+            return false;
+
+        if (pesos >= weaponPrices[weapon.weaponLevel])
+        {
+            pesos -= weaponPrices[weapon.weaponLevel];
+            weapon.UpgradeWeapon();
+            return true;
+        }
+        return false;
     }
 
     // Save State
@@ -54,7 +70,7 @@ public class GameManager : MonoBehaviour
         s += "0" + "|";
         s += pesos.ToString() + "|";
         s += experience.ToString() + "|";
-        s += "0";
+        s += weapon.weaponLevel.ToString();
 
         PlayerPrefs.SetString("SaveState", s);
     }
@@ -69,8 +85,8 @@ public class GameManager : MonoBehaviour
         pesos = int.Parse(data[1]);
         experience = int.Parse(data[2]);
         // Change the weapon Level
+        weapon.SetWeaponLevel (int.Parse(data[3]));
 
         Debug.Log("LoadState");
     }
-
 }
